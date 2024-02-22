a3b5422cc59fd8b71f57aab2fcd18edba155f34a
mingwiki <mingwiki@gmail.com>
2024-02-22 15:57:30 +0800

    update

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -32,8 +32,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<A-l>", ":bnext<CR>", opts)
+keymap("n", "<A-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", ":m .+1<CR>==", opts)
