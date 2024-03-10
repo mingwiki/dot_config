50c23d3945fc7bb5575296f4ceed54c7b3147f37
mingwiki <mingwiki@gmail.com>
2024-03-10 19:26:59 +0800

    update

--- nvim/lua/lsp/lspconfig.lua
+++ nvim/lua/lsp/lspconfig.lua
@@ -106,7 +106,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { "bashls", "pyright", "biome" }
+local servers = { "bashls", "pyright", "jsonls", "tsserver", "remark_ls" }
 
 -- Call setup
 for _, lsp in ipairs(servers) do
