85fb0f02fb6d53195d35a8f41048b53b9b012c60
mingwiki <mingwiki@gmail.com>
2024-03-13 11:55:06 +0800

    update

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -34,7 +34,7 @@ lazy.setup({
 			"navarasu/onedark.nvim",
 			lazy = false, -- make sure we load this during startup if it is your main colorscheme
 			priority = 1000, -- make sure to load this before all the other start plugins
-		},          -- Icons
+		}, -- Icons
 		{
 			"kyazdani42/nvim-web-devicons",
 			lazy = true,
@@ -55,7 +55,7 @@ lazy.setup({
 			config = function()
 				require("nvim-autopairs").setup({})
 			end,
-		},                     -- LSP
+		}, -- LSP
 		{ "neovim/nvim-lspconfig" }, -- Autocomplete
 		{
 			"hrsh7th/nvim-cmp",
