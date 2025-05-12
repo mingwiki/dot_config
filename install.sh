72119b42a8436a411137417550963fa5bbdfb36a
mingwiki <mingwiki@gmail.com>
2025-05-12 09:51:45 +0800

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,2 @@
 ln -sf ~/.config/.gitconfig ~/.gitconfig
-ln -sf ~/.config/.profile ~/.profile
 ln -sf ~/.config/.npmrc ~/.npmrc
-ln -sf ~/.config/.vimrc ~/.vimrc
-ln -sf ~/.config/cargo.toml ~/.cargo/config.toml
